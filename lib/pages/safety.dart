95ccfccac670e6b12fac4e75b6ecd413ad9c5291
kagaminearia <rinkitang@outlook.com>
2024-11-13 21:16:00 -0800

    add safety main page and overlay

---
+++ lib/pages/safety.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:odyssey/pages/safety_sos.dart';
+
+class Safety extends StatelessWidget{
+  const Safety({super.key});
+
+  // make all buttons with same style
+  Widget buildButton(String text, VoidCallback onPressed, double width) {
+    return SizedBox(
+      width: width,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color(0xFF006A68),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        child: Text(text),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // set the fixed width for buttons based on the longest text
+    double buttonWidth = 200; 
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 189, 220, 204),
+        title: Text("Travel Safety"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            buildButton("Location Checkin", () {
+              
+            }, buttonWidth),
+            SizedBox(height: 20),
+            buildButton("Emergency Contact", () {
+              
+            }, buttonWidth),
+            SizedBox(height: 20),
+            buildButton("Solo Safety Tips", () {
+              
+            }, buttonWidth),
+            SizedBox(height: 20),
+            buildButton("Live Sharing", () {
+              
+            }, buttonWidth),
+            SizedBox(height: 20),
+            buildButton("SOS", () {
+              showOverlay(context); 
+            }, buttonWidth),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/safety_sos.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void showOverlay(BuildContext context) {
+    late OverlayEntry overlayEntry;
+
+    // Define the overlay entry and close button action
+    overlayEntry = OverlayEntry(
+      builder: (context) => Positioned(
+        top: MediaQuery.of(context).size.height * 0.3, 
+        left: MediaQuery.of(context).size.width * 0.1,
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: Material(
+          color: Colors.transparent,
+          child: Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  blurRadius: 10,
+                  offset: Offset(0, 5),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Do you want to call SOS",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        overlayEntry.remove();
+                      },
+                      child: Text("Yes"),
+                    ),
+                    SizedBox(width: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        overlayEntry.remove();
+                      },
+                      child: Text("No"),
+                    ),
+                  ],
+                ),
+                
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+
+
+    // Insert the overlay into the Overlay widget
+    Overlay.of(context)!.insert(overlayEntry);
+  }
