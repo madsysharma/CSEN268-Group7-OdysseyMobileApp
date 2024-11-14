87c54bb69e4d9c43fcd3ca0787a250fd7c6ea42c
Pujitha Kallu <pkallu@scu.edu>
2024-11-13 20:21:45 -0800

    changes to podfiles

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    [GMSServices provideAPIKey:@"AIzaSyDblK5p5-rCV764KfMi5yYpr8u3X9thGcQ"];
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
