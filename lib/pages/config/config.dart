c5b6d80c35671263fe3be409b7d587d9fdbfe3be
harshini259 <harshinijonnala1999@gmail.com>
2024-12-01 17:43:39 -0800

    "Changes from https://github.com/madsysharma/CSEN268-Group7-OdysseyMobileApp/pull/9"

---
+++ lib/pages/config/config.dart
@@ -0,0 +1,5 @@
+class Config {
+  // Google Maps API Key
+  static const String googleApiKey = 'AIzaSyDGbl_R3u5F1A9hVlpzDxF6AVgQMHp4hwM';
+
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,9 +14,14 @@ import firebase_core
 import firebase_storage
 import geolocator_apple
 import google_sign_in_ios
+import geolocator_apple
 import location
 import path_provider_foundation
+import share_plus
 import shared_preferences_foundation
+import url_launcher_macos
+import speech_to_text_macos
+import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -29,8 +34,13 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
