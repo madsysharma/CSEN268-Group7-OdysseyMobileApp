256da974fd1017187e58e7e0bee934e478f4df35
harshini259 <harshinijonnala1999@gmail.com>
2024-12-02 19:02:24 -0800

    Adding display name

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:odyssey/bloc/auth/auth_bloc.dart';
 import 'package:odyssey/bloc/locations/locations_bloc.dart';
 import 'package:odyssey/components/home_locations_list.dart';
 import 'package:odyssey/components/search_places.dart';
+
+FirebaseAuth auth = FirebaseAuth.instance;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -21,14 +24,14 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    var user = (context.read<AuthBloc>().state as LoggedIn).user;
-
+    final double topInset = MediaQuery.of(context).viewPadding.top;
+    var userName = auth.currentUser?.displayName ?? auth.currentUser?.email;
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: EdgeInsets.only(top: topInset + 16.0, left: 16, right:16 , bottom :16),
       child: Column(
         children: [
           Text(
-            "Hi ${user.name}!",
+            "Hi ${userName}!",
             style: Theme.of(context).textTheme.titleLarge,
             textAlign: TextAlign.left,
           ),
