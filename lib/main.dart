4b5bdcd82a5816aa45cc01ce95fd1c5f02c5d75e
spolkampalli <spolkampalli@scu.edu>
2024-12-03 20:26:53 -0800

    Changed main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,36 +8,33 @@ import 'package:odyssey/bloc/locationDetails/location_details_bloc.dart';
 import 'package:odyssey/bloc/locations/locations_bloc.dart';
 import 'package:odyssey/components/navigation/shell_bottom_nav_bar.dart';
 import 'package:odyssey/model/location.dart';
-import 'package:odyssey/pages/connect/expired_request.dart';
-import 'package:odyssey/pages/profile/download_network.dart';
-import 'package:odyssey/pages/profile/edit_profile.dart';
 import 'package:odyssey/pages/home.dart';
-//import 'package:odyssey/pages/locc3
 import 'package:odyssey/pages/maps/map_page.dart';
 import 'package:odyssey/pages/location_details/location_details.dart';
 import 'package:odyssey/pages/profile/forgotpass_page.dart';
-import 'package:odyssey/pages/profile/main_page.dart';
 import 'package:odyssey/pages/profile/login_page.dart';
-import 'package:odyssey/pages/profile/manage_membership.dart';
 import 'package:odyssey/pages/profile/profile_page.dart';
+import 'package:odyssey/pages/profile/edit_profile.dart';
 import 'package:odyssey/pages/profile/saved_locations.dart';
+import 'package:odyssey/pages/profile/download_network.dart';
+import 'package:odyssey/pages/profile/manage_membership.dart';
+import 'package:odyssey/pages/connect/connect.dart';
+import 'package:odyssey/pages/connect/friend_request.dart';
+import 'package:odyssey/pages/connect/notifications.dart';
+import 'package:odyssey/pages/connect/accept_request.dart';
+import 'package:odyssey/pages/connect/expired_request.dart';
+import 'package:odyssey/pages/connect/upload_post.dart';
 import 'package:odyssey/pages/profile/signup_page.dart';
+import 'package:odyssey/utils/custom_gallery.dart';
 import 'package:odyssey/pages/safety/safety.dart';
 import 'package:odyssey/pages/safety/safety_checkin.dart';
 import 'package:odyssey/pages/safety/safety_emer.dart';
 import 'package:odyssey/pages/safety/safety_sharing.dart';
 import 'package:odyssey/pages/safety/safety_tips.dart';
-import 'package:odyssey/pages/connect/connect.dart';
-import 'package:odyssey/pages/connect/friend_request.dart';
-import 'package:odyssey/pages/connect/notifications.dart';
-import 'package:odyssey/pages/connect/accept_request.dart';
-import 'package:odyssey/utils/custom_gallery.dart';
 import 'package:odyssey/utils/paths.dart';
-import 'package:odyssey/pages/connect/upload_post.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
-
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -45,7 +42,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await FirebaseAppCheck.instance.activate(
-    androidProvider: AndroidProvider.debug
+    androidProvider: AndroidProvider.debug,
   );
   runApp(MultiBlocProvider(
     providers: [
@@ -59,153 +56,171 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
+
   MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Determine initial location based on authentication state
     final isLoggedIn = FirebaseAuth.instance.currentUser != null;
-    final initialLocation = isLoggedIn ? Paths.home : Paths.mainPage;
+    final initialLocation = isLoggedIn ? Paths.home : Paths.loginPage;
 
-    // Set up GoRouter
     final GoRouter router = GoRouter(
-        initialLocation: Paths.loginPage,
-        routes: [
-          ShellRoute(
-            builder: (context, state, child) => ShellBottomNavBar(child: child),
-            routes: [
-              GoRoute(
-                path: Paths.home,
-                builder: (context, state) => HomeScreen(),
-              ),
-              GoRoute(
-                  path: Paths.connect,
-                  builder: (context, state) => Connect(tab: 'local'),
-                  routes: [
-                    GoRoute(
-                        path: 'local',
-                        builder: (context, state) => Connect(
-                              tab: 'local',
-                            ),
-                        routes: [
-                          GoRoute(
-                            path: Paths.friendReq,
-                            builder: (context, state) => FriendRequest(),
-                          ),
-                        ]),
-                    GoRoute(
-                        path: 'friends',
-                        builder: (context, state) => Connect(
-                              tab: 'friends',
-                            ),
-                        routes: [
-                          GoRoute(
-                            path: Paths.friendReq,
-                            builder: (context, state) => FriendRequest(),
-                          ),
-                        ]),
-                    GoRoute(
-                        path: 'you',
-                        builder: (context, state) => Connect(
-                              tab: 'you',
-                            ),
-                        routes: [
-                          GoRoute(
-                            path: Paths.post,
-                            builder: (context, state) => UploadPost(),
-                          ),
-                          GoRoute(
-                            path: Paths.friendReq,
-                            builder: (context, state) => FriendRequest(),
-                          ),
-                        ]),
-                  ]),
-              GoRoute(
-                path: Paths.maps,
-                builder: (context, state) => MapPage(),
-              ),
-              GoRoute(
-                path: Paths.safety,
-                builder: (context, state) => Safety(),
-                routes: [
-                  GoRoute(
-                    path: 'location-checkin',
-                    builder: (context, state) => SafetyCheckin(),
-                  ),
-                  GoRoute(
-                    path: 'emergency-contact',
-                    builder: (context, state) => ContactsPage(),
-                  ),
-                  GoRoute(
-                    path: 'travel-tips',
-                    builder: (context, state) => SafetyTips(),
-                  ),
-                  GoRoute(
-                    path: 'sharing',
-                    builder: (context, state) => SharingPage(),
-                  ),
-                ],
-              ),
-              GoRoute(
-                path: Paths.profile,
-                builder: (context, state) => ProfileScreen(),
-              ),
-              GoRoute(
-                path: Paths.profilePage,
-                builder: (context, state) => ProfilePage(),
-              ),
-              GoRoute(
-                path: Paths.editProfile,
-                builder: (context, state) => EditProfilePage(),
-              ),
-              GoRoute(
-                path: Paths.savedLocations,
-                builder: (context, state) => SavedLocations(),
-              ),
-              GoRoute(
-                path: Paths.downloadNetwork,
-                builder: (context, state) => DownloadNetworkPage(),
-              ),
-              GoRoute(
-                path: Paths.manageMembership,
-                builder: (context, state) => ManageMembership(),
-              ),
-              GoRoute(
-                path: Paths.signupPage,
-                builder: (context, state) => SignUpPage(),
-              ),
-              GoRoute(
-                path: Paths.forgotPassword,
-                builder: (context, state) => ForgotPasswordPage(),
-              ),
-            ],
-          ),
-          GoRoute(
+      initialLocation: initialLocation,
+      routes: [
+        ShellRoute(
+          builder: (context, state, child) => ShellBottomNavBar(child: child),
+          routes: [
+            GoRoute(
+              path: Paths.home,
+              builder: (context, state) => HomeScreen(),
+            ),
+            GoRoute(
               path: Paths.locationDetails,
               builder: (context, state) {
                 var location = state.extra as String;
                 return LocationDetailsPage(locationId: location);
-              }),
-          GoRoute(
-            path: Paths.loginPage,
-            builder: (context, state) => LoginPage(),
-          ),
-        ],
-        redirect: (context, state) {
-          var loggedIn = context.read<AuthBloc>().state is LoggedIn;
-          var tryingToLogin = state.fullPath == Paths.loginPage;
-          var tryingToSignup = state.fullPath == Paths.signupPage;
-          var tryingToUpdatePassword = state.fullPath == Paths.forgotPassword;
+              },
+            ),
+            GoRoute(
+              path: Paths.connect,
+              builder: (context, state) => Connect(tab: 'local'),
+              routes: [
+                GoRoute(
+                  path: 'local',
+                  builder: (context, state) => Connect(tab: 'local'),
+                  routes: [
+                    GoRoute(
+                      path: Paths.friendReq,
+                      builder: (context, state) => FriendRequest(),
+                    ),
+                    GoRoute(
+                      path: Paths.notifs,
+                      builder: (context, state) => Notifications(fromScreen: 'local'),
+                      routes: [
+                        GoRoute(
+                          path: Paths.acceptReq,
+                          builder: (context, state) {
+                            final query = state.uri.queryParameters['q'];
+                            return AcceptRequest(requesterName: query);
+                          },
+                        ),
+                        GoRoute(
+                          path: Paths.expiredReq,
+                          builder: (context, state) => ExpiredRequest(),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                GoRoute(
+                  path: 'friends',
+                  builder: (context, state) => Connect(tab: 'friends'),
+                  routes: [
+                    GoRoute(
+                      path: Paths.friendReq,
+                      builder: (context, state) => FriendRequest(),
+                    ),
+                    GoRoute(
+                      path: Paths.notifs,
+                      builder: (context, state) => Notifications(fromScreen: 'friends'),
+                    ),
+                  ],
+                ),
+                GoRoute(
+                  path: 'you',
+                  builder: (context, state) => Connect(tab: 'you'),
+                  routes: [
+                    GoRoute(
+                      path: Paths.post,
+                      builder: (context, state) {
+                        final location = state.extra as LocationDetails?;
+                        return UploadPostInitial(location: location);
+                      },
+                      routes: [
+                        GoRoute(
+                          path: Paths.customGallery,
+                          builder: (context, state) =>
+                              CustomGallery(imgPaths: state.extra as List<String>),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            GoRoute(
+              path: Paths.maps,
+              builder: (context, state) => MapPage(),
+            ),
+            GoRoute(
+              path: Paths.safety,
+              builder: (context, state) => Safety(),
+              routes: [
+                GoRoute(
+                  path: 'location-checkin',
+                  builder: (context, state) => SafetyCheckin(),
+                ),
+                GoRoute(
+                  path: 'emergency-contact',
+                  builder: (context, state) => ContactsPage(),
+                ),
+                GoRoute(
+                  path: 'travel-tips',
+                  builder: (context, state) => SafetyTips(),
+                ),
+                GoRoute(
+                  path: 'sharing',
+                  builder: (context, state) => SharingPage(),
+                ),
+              ],
+            ),
+            GoRoute(
+              path: Paths.profilePage,
+              builder: (context, state) => ProfilePage(),
+            ),
+            GoRoute(
+              path: Paths.editProfile,
+              builder: (context, state) => EditProfilePage(),
+            ),
+            GoRoute(
+              path: Paths.savedLocations,
+              builder: (context, state) => SavedLocations(),
+            ),
+            GoRoute(
+              path: Paths.downloadNetwork,
+              builder: (context, state) => DownloadNetworkPage(),
+            ),
+            GoRoute(
+              path: Paths.manageMembership,
+              builder: (context, state) => ManageMembership(),
+            ),
+            GoRoute(
+              path: Paths.signupPage,
+              builder: (context, state) => SignUpPage(),
+            ),
+            GoRoute(
+              path: Paths.forgotPassword,
+              builder: (context, state) => ForgotPasswordPage(),
+            ),
+          ],
+        ),
+        GoRoute(
+          path: Paths.loginPage,
+          builder: (context, state) => LoginPage(),
+        ),
+      ],
+      redirect: (context, state) {
+        final loggedIn = context.read<AuthBloc>().state is LoggedIn;
+        final tryingToLogin = state.matchedLocation == Paths.loginPage;
+        final tryingToSignup = state.matchedLocation == Paths.signupPage;
+        final tryingToUpdatePassword = state.matchedLocation == Paths.forgotPassword;
 
-        // Redirect logic based on authentication
-        if (loggedIn &&
-            (tryingToLogin || tryingToSignup || tryingToUpdatePassword)) {
+        if (loggedIn && (tryingToLogin || tryingToSignup || tryingToUpdatePassword)) {
           return Paths.home;
         }
 
-        if (!loggedIn &&
-            !(tryingToLogin || tryingToSignup || tryingToUpdatePassword)) {
-          return Paths.mainPage;
+        if (!loggedIn && !(tryingToLogin || tryingToSignup || tryingToUpdatePassword)) {
+          return Paths.loginPage;
         }
         return null;
       },
@@ -217,7 +232,7 @@ class MyApp extends StatelessWidget {
         if (state is LoggedIn) {
           router.go(Paths.home);
         } else if (state is LoggedOut) {
-          router.go(Paths.mainPage);
+          router.go(Paths.loginPage);
         }
       },
       child: MaterialApp.router(
@@ -228,48 +243,8 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           floatingActionButtonTheme: FloatingActionButtonThemeData(
             backgroundColor: const Color(0xFF006A68),
-            foregroundColor: const Color(0xFFFFFFFF),
+            foregroundColor: Colors.white,
             elevation: 4,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ButtonStyle(
-              backgroundColor: WidgetStateProperty.all(
-                const Color(0xFF006A68),
-              ),
-              foregroundColor: WidgetStateProperty.all(
-                const Color(0xFFFFFFFF),
-              ),
-              padding: WidgetStateProperty.all<EdgeInsets>(
-                const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              ),
-              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              minimumSize: WidgetStateProperty.all<Size>(
-                const Size(130, 50),
-              ),
-            ),
-          ),
-          textButtonTheme: TextButtonThemeData(
-              style: ButtonStyle(
-            foregroundColor: WidgetStateProperty.all(const Color(0xFF006A68)),
-            padding: WidgetStateProperty.all(
-              const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-            ),
-          )),
-          snackBarTheme: SnackBarThemeData(
-            backgroundColor: const Color(0xFF3F4948),
-            actionTextColor: const Color(0xFFF4FBF9),
-            contentTextStyle: const TextStyle(
-              color: Color(0xFFF4FBF9),
-            ),
-            elevation: 4,
-            behavior: SnackBarBehavior.floating,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10),
             ),
